0397f355bc5fe8728ff16ca890c80231f20df195
Mr. Kutin <serge.kutin@gmail.com>
2023-12-13 18:05:22 +0300

    locenses insert

--- services/storage/index.mjs
+++ services/storage/index.mjs
@@ -3,6 +3,11 @@ import mysqlAdapter from './adapters/mysql.mjs'
 
 const processor = async ({topic, partition, message}) => {
     const {action, entity} = JSON.parse(message.value.toString())
+
+    if(entity.expires){
+        entity.expires = entity.expires.replace('T', ' ').replace('Z', '')
+    }
+
     switch (action) {
         case 'create':
             await mysqlAdapter.create(topic, entity)
